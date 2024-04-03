806f3ab2bf9a6ab902e6c0cab7b505e8d0633577
LeoG123321 <3545gonzalez@gmail.com>
2024-04-02 20:02:48 -0500

    Add project files.

---
+++ a_simple_todo_list/TodoList.h
@@ -0,0 +1,14 @@
+#pragma once
+class TodoList
+{
+public:
+	TodoList();
+
+	void add_task();
+	void complete_task();
+	void all_tasks();
+	void completed_task_list();
+	void incomplete_task_list();
+	void clear();
+};
+
---
+++ a_simple_todo_list/a_simple_todo_list.cpp
@@ -0,0 +1,14 @@
+/*
+    Leonardo Gonzalez   4/2/2024
+    
+    Week 12             A Simple Todo List
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
